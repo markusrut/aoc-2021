f44f323d29822dbe803c6b5f5a0e7c2bd015f0f7
Markus Rutström <markusrutstrom@gmail.com>
2021-12-06 21:26:32 +0100

    Day 6

--- 06/script.py
+++ 06/script.py
@@ -2,36 +2,45 @@ import os
 from typing import List
 
 
-def iterate_days(fishes: List[int], days: int):
-    reset_fish = 6
-    new_fish = 8
+def parse_input(input: str):
+    fish_input = list(map(int, input.split(',')))
+    fish_count = [0] * 9
+    for fish in fish_input:
+        fish_count[fish] += 1
+    return fish_count
 
-    for d in range(days):
+
+def iterate_days(fish_count: List[int], days: int):
+    reset_fish_index = 6
+    new_fish_index = 8
+
+    for _ in range(days):
         spawn_count = 0
-        for i in range(len(fishes)):
-            if fishes[i] > 0:
-                fishes[i] -= 1
-            else:
-                spawn_count += 1
-                fishes[i] = reset_fish
+        for i in range(len(fish_count)):
+            if i == 0:
+                spawn_count = fish_count[i]
+            elif i > 0:
+                fish_count[i-1] = fish_count[i]
 
-        new_fishes = [new_fish] * spawn_count
-        fishes = fishes + new_fishes
-    return fishes
+        fish_count[new_fish_index] = spawn_count
+        fish_count[reset_fish_index] += spawn_count
+        # print(f"Day: {d+1}, Fish count: {fish_count}, Sum: {sum(fish_count)}")
 
+
+def part1(input: str):
+    fish_count = parse_input(input)
+    iterate_days(fish_count, 80)
+    print(sum(fish_count))
+    
+def part2(input: str):
+    fish_count = parse_input(input)
+    iterate_days(fish_count, 256)
+    print(sum(fish_count))
 
 os.chdir('06')
 with open("input.txt", "r") as f:
-    input = [line.rstrip() for line in f.readlines()]
-    fish_input = list(map(int, input[0].split(',')))
+    input = f.readline().rstrip()
+    # input = [line.rstrip() for line in f.readlines()]
 
-def part1():
-    fishes = iterate_days(fish_input, 80)
-    print(len(fishes))
-    
-def part2():
-    fishes = iterate_days(fish_input, 256)
-    print(len(fishes))
-
-part1()
-# part2()
+part1(input)
+part2(input)
